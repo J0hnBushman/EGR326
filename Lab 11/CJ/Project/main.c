3ffe4233586fb690be08b37bb1c3691fe9381892
K1CKFL1P <charlespnortonjr@gmail.com>
2024-11-14 22:12:47 -0500

    Full send on the EEPROM Program

--- Lab 11/CJ/Project/main.c
+++ Lab 11/CJ/Project/main.c
@@ -1,36 +1,31 @@
 #include "stm32f446xx.h"
-#include "EEPROM.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "UART.h"
+#include "I2C.h"
+#include "systick.h"
 /*=============================================================
 Name: 				Bushman & Norton
 
 Course:				EGR 326 - Embedded Systems
 
-Project:			Integration with an RTC using an I2C Bus
+Project:			RTC and an EEPROM using an I2C Bus
 
 File: 				main.c
 
-Description: 	When the user enters a ?*? followed by a ?1? on the keypad, 
-							the current day, month, and year are displayed on the serial 
-							monitor.  
-							When the user enters a ?*? followed by a ?2? on the 
-							keypad, the hour, minute, and second are displayed.
-							If the user enters a ?*? followed by a ?3? on the keypad, the 
-							temperature is displayed in Celsius
+Description: 	
 =============================================================*/
 
-#include "stm32f446xx.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
-#include "UART.h"
-#include "I2C.h"
+
+
+#define BP1 ((GPIOC->IDR)&(1<<13))	
+#define BP2 ((GPIOC->IDR)&(1<<11))
 
 #define MSB(X)          (((X & 0x70)>>4)+'0')
 #define Y_MSB(X)        (((X & 0xF0)>>4)+'0')
 #define LSB(X)          ((X & 0x0F)+'0')
-#define SLAVE_ADDR      0x68
+#define RTC_SLAVE_ADDR      0x68
+#define EEPROM_SLAVE_ADDR 0x50
 #define SECONDS_ADDR    0x00
 #define MINUTES_ADDR    0x01
 #define HOURS_ADDR      0x02
@@ -42,83 +37,154 @@ Description: 	When the user enters a ?*? followed by a ?1? on the keypad,
 #define TEMP_ADDR_WHOLE 0x11
 #define TEMP_ADDR_FRAC  0x12
 
-void delayMs(int n);
-void Read_Time(void);
-void Read_Date(void);
-void Set_Time(void);
-void Set_Date(void);
-void Read_Temperature(void);
-void menu_choice(void);
+// EEPROM MEM addresses for different Times
+#define TIME1_HR		0x00
+#define TIME1_MIN		0x08
+#define TIME1_SEC		0x10
 
-uint8_t num = 0;
-uint8_t choice = 0;
-uint16_t pressed;
-uint8_t *numptr = &num;
-bool flag = 0;
-char S_address = 0x68;
+#define TIME2_HR		0x18
+#define TIME2_MIN		0x20
+#define TIME2_SEC		0x28
+
+#define TIME3_HR		0x30
+#define TIME3_MIN		0x38
+#define TIME3_SEC	  0x40
+
+#define TIME4_HR		0x48
+#define TIME4_MIN		0x50
+#define TIME4_SEC		0x58
+
+#define TIME5_HR		0x60
+#define TIME5_MIN		0x68		
+#define TIME5_SEC		0x70
+
+void Read_Time(void);
+void Store_Time(void);
+void Read_Date(void);
+void btn_init(void);
+
+
+uint8_t entry = 0;
 char RTC_data;
 char *RTC_Data_ptr = &RTC_data;
 
-uint8_t RTC_Hour;
-uint8_t RTC_Minute;
-uint8_t RTC_Second;
-
-uint8_t RTC_Year;
-uint8_t RTC_Month;
-uint8_t RTC_Date;
-uint8_t RTC_Day;
+    uint8_t time[5][3] = {
+        {TIME1_HR, TIME1_MIN, TIME1_SEC},  // TIME1
+        {TIME2_HR, TIME2_MIN, TIME2_SEC},  // TIME2
+        {TIME3_HR, TIME3_MIN, TIME3_SEC},  // TIME3
+        {TIME4_HR, TIME4_MIN, TIME4_SEC},  // TIME4
+        {TIME5_HR, TIME5_MIN, TIME5_SEC}   // TIME5
+    };
 
 /////////////////////////////////////////////////////////////////////
 
 int main (void){
+	SysTick_Init();
 	I2C_init();
 	USART2_init();
+	btn_init();
 	
-
-	
-	USART2_print("Thank you for purchasing Bushman and Nortons Real Time Clock!\n\r\n");
-	USART2_print("Press '#' key to change time and date.\n\r\n");
 	while(1){
-//			I2C1_byteWrite(SLAVE_ADDR,HOURS_ADDR,RTC_Hour);
-//		I2C1_byteWrite(SLAVE_ADDR,MINUTES_ADDR,RTC_Minute);
-//		I2C1_byteWrite(SLAVE_ADDR,SECONDS_ADDR,RTC_Second);
 		
+		if(BP1==0){//store time in EEPROM
+			while(!BP1);
+			Store_Time();
+		}
+		
+		if(BP2==0){//read time from EEPROM
+			while(!BP2);
+			Read_Time();
+		}
 		
 	}
-
 	return 0;
+}
+/*===========================================================================================
+// Store_Time
+
+ * @brief: Stores the time from the RTC on to EEPROM
+ * 
+ * @param[in] NULL
+ * 
+ * @return: NULL
+ ===========================================================================================*/
+void Store_Time(void){
+	volatile int i = 0;
+	
+	delayMS(1);
+    // Read the current hour from the RTC and store in RTC_Data_ptr
+	I2C1_byteRead(RTC_SLAVE_ADDR, HOURS_ADDR, RTC_Data_ptr);
+	delayMS(1);
+		// Write the data from the RTC to a memory address in the EEPROM
+	I2C1_byteWrite(EEPROM_SLAVE_ADDR, time[entry][i++], RTC_data);
+   delayMS(1);
+	
+    // Read the current minutes from the RTC
+	I2C1_byteRead(RTC_SLAVE_ADDR, MINUTES_ADDR, RTC_Data_ptr);
+	delayMS(1);
+		// Write the data from the RTC to a memory address in the EEPROM
+	I2C1_byteWrite(EEPROM_SLAVE_ADDR, time[entry][i++], RTC_data);
+   delayMS(1);
+	
+    // Read the current seconds from the RTC
+	I2C1_byteRead(RTC_SLAVE_ADDR, SECONDS_ADDR, RTC_Data_ptr);
+	delayMS(1);
+		// Write the data from the RTC to a memory address in the EEPROM
+	I2C1_byteWrite(EEPROM_SLAVE_ADDR, time[entry][i], RTC_data);
+	delayMS(1);
+    
+	
+	entry = (entry>=4)? 0 : entry+1;
 }
 
 void Read_Time(void){
-    // Read the current hour from the RTC and store in RTC_Data_ptr
-	I2C1_byteRead(SLAVE_ADDR, HOURS_ADDR, RTC_Data_ptr);
+	volatile int i = 0;
+	
+	USART2_print("Entry: ");
+	USART2_write((entry+1)+'0');
+	USART2_print("\n\r");
+	delayMS(1);
+    // Read the current hour from the current entry in EEPROM and store in RTC_Data_ptr
+	I2C1_byteRead(EEPROM_SLAVE_ADDR, time[entry][i++], RTC_Data_ptr);
+	
+	delayMS(1);
     
     // Send the hour to USART2 (most significant byte and least significant byte)
 	USART2_write(MSB(RTC_data));
 	USART2_write(LSB(RTC_data));
 	USART2_write(':');  // Add colon between hours and minutes
 	
-    // Read the current minutes from the RTC
-	I2C1_byteRead(SLAVE_ADDR, MINUTES_ADDR, RTC_Data_ptr);
-    
+	delayMS(1);
+		// Read the current minute from the current entry in EEPROM and store in RTC_Data_ptr
+	I2C1_byteRead(EEPROM_SLAVE_ADDR, time[entry][i++], RTC_Data_ptr);
+  
+	delayMS(1);
+	
     // Send the minutes to USART2
 	USART2_write(MSB(RTC_data));
 	USART2_write(LSB(RTC_data));
 	USART2_write(':');  // Add colon between minutes and seconds
 	
-    // Read the current seconds from the RTC
-	I2C1_byteRead(SLAVE_ADDR, SECONDS_ADDR, RTC_Data_ptr);
-    
+	delayMS(1);
+	
+	// Read the current second from the current entry in EEPROM and store in RTC_Data_ptr
+	I2C1_byteRead(EEPROM_SLAVE_ADDR, time[entry][i++], RTC_Data_ptr);
+   
+	delayMS(1);
+	
     // Send the seconds to USART2
 	USART2_write(MSB(RTC_data));
 	USART2_write(LSB(RTC_data));
     
     // Print a newline to finish the time output
 	USART2_print("\n\r\n");
+	
+	delayMS(1);
+	entry = (entry>=4)? 0 : entry+1;
 }
 
 /*===========================================================================================
-// Read_Date
+// button_init
 
  * @brief: Prints the current date stored on the RTC
  * 
@@ -126,129 +192,11 @@ void Read_Time(void){
  * 
  * @return: NULL
  ===========================================================================================*/
-void Read_Date(void){
-    // Read the day of the week from the RTC
-	I2C1_byteRead(SLAVE_ADDR, DAY_ADDR, RTC_Data_ptr);
-    
-    // Print the day of the week based on the value of RTC_data
-	if (RTC_data == 1) USART2_print("Sunday ");
-	if (RTC_data == 2) USART2_print("Monday ");
-	if (RTC_data == 3) USART2_print("Tuesday ");
-	if (RTC_data == 4) USART2_print("Wednesday ");
-	if (RTC_data == 5) USART2_print("Thursday ");
-	if (RTC_data == 6) USART2_print("Friday ");
-	if (RTC_data == 7) USART2_print("Saturday ");
-	
-    // Read the current month from the RTC and display it
-	I2C1_byteRead(SLAVE_ADDR, MONTH_ADDR, RTC_Data_ptr);
-	USART2_write(MSB(RTC_data));  // Print the most significant byte (MSB)
-	USART2_write(LSB(RTC_data));  // Print the least significant byte (LSB)
-	USART2_write('/');  // Separator between month and date
-	
-    // Read the current date (day of the month) from the RTC and display it
-	I2C1_byteRead(SLAVE_ADDR, DATE_ADDR, RTC_Data_ptr);
-	USART2_write(MSB(RTC_data));
-	USART2_write(LSB(RTC_data));
-	USART2_write('/');  // Separator between date and year
-	
-    // Read the year from the RTC and display it
-	I2C1_byteRead(SLAVE_ADDR, YEAR_ADDR, RTC_Data_ptr);
-	USART2_write(Y_MSB(RTC_data));  // Print the most significant byte of the year
-	USART2_write(LSB(RTC_data));    // Print the least significant byte of the year
-	
-    // Print a newline to finish the date output
-	USART2_print("\n\r\n");
-}
-
-/*===========================================================================================
-// Read_Temperature
-
- * @brief: Reads and prints the temperature from the RTC
- * 
- * @param[in] NULL
- * 
- * @return: NULL
- ===========================================================================================*/
-void Read_Temperature(void){
-    // Declare variables to hold temperature data in different formats
-	int8_t temperature = 0;
-	int8_t temp_hundreds = 0;
-	int8_t temp_tens = 0;
-	int8_t temp_ones = 0;
-	
-    // Print the initial "Temperature: " string
-	USART2_print("Temperature: ");
-	
-    // Read the whole temperature value (whole part of the temperature)
-	I2C1_byteRead(SLAVE_ADDR, TEMP_ADDR_WHOLE, RTC_Data_ptr);
-
-    // Assign the temperature value to the variable
-	temperature = (int8_t)RTC_data;
-	
-    // Check if the temperature is negative (bit 7 indicates sign)
-	if (((RTC_data & 0x80) >> 7) == 0x01) {  // Temperature is negative
-        USART2_write('-');  // Print negative sign
-        
-        // Extract the hundreds, tens, and ones digits
-		temp_hundreds = temperature / (-100);
-		if (temp_hundreds != 0) USART2_write(((char)temp_hundreds + '0'));  // Print hundreds digit
-		temp_tens = temperature / (-10);
-		if (temp_tens != 0 || temp_hundreds != 0) USART2_write(((char)temp_tens + '0'));  // Print tens digit
-		temp_ones = temperature % (-10);
-		USART2_write(((char)temp_ones + '0'));  // Print ones digit
-	}
-	else {  // Temperature is positive
-        temp_hundreds = temperature / 100;
-		if (temp_hundreds != 0) USART2_write(((char)temp_hundreds + '0'));  // Print hundreds digit
-		temp_tens = temperature / 10;
-		if (temp_tens != 0 || temp_hundreds != 0) USART2_write(((char)temp_tens + '0'));  // Print tens digit
-		temp_ones = temperature % 10;
-		USART2_write(((char)temp_ones + '0'));  // Print ones digit
-	}
-	
-    // Print the decimal point
-	USART2_write(RTC_data);
-	USART2_write('.');
-
-    // Read the fractional part of the temperature
-	I2C1_byteRead(SLAVE_ADDR, TEMP_ADDR_FRAC, RTC_Data_ptr);
-	
-    // Convert the fractional value to human-readable format and print
-	RTC_data = (RTC_data & 0x0C) >> 6;  // Extract the two most significant bits (for fractions)
-	if (RTC_data == 0x00) USART2_print("00");
-	if (RTC_data == 0x01) USART2_print("25");
-	if (RTC_data == 0x02) USART2_print("50");
-	if (RTC_data == 0x03) USART2_print("75");
-	
-    // Print the unit (Degrees Celsius)
-	USART2_print(" Degrees Celsius\n\r\n");
-}
-
-/*===========================================================================================
-// menu_choice
-
- * @brief: Prompts the user to navigate through a menu
- * 
- * @param[in] NULL
- * 
- * @return: NULL
- ===========================================================================================*/
-void menu_choice(void){
-    // Initialize pressed flag to zero (user hasn't pressed a key yet)
-	pressed = 0;
-	
-    // Display menu options on the terminal
-	USART2_print("Press 1 for ................ Date\n\r");
-	USART2_print("Press 2 for ................ Time\n\r");
-	USART2_print("Press 3 for ................ Temperature\n\r\n");
-
-    // Wait until the user presses a key (keypad input)
-	while (pressed == 0) {
-        pressed = Read_Keypad(numptr);  // Check if a key is pressed
-        
-        // Depending on the key pressed, call the appropriate function
-		if (num == 1) { Read_Date(); }
-		if (num == 2) { Read_Time(); }
-		if (num == 3) { Read_Temperature(); }
-	}
+void btn_init(void){
+	RCC->AHB1ENR |= 0x7;
+	//PC13, 11 for button press
+	GPIOC->MODER &= ~ (0x03<<(2*13));
+	GPIOC->MODER &= ~ (0x03<<(2*11));
+	GPIOC->PUPDR |=   0x01<<(13*2);
+	GPIOC->PUPDR |=   0x01<<(11*2);
 }
